d0d21b2a141339b8014e820515e5921942dab2a0
Frans <97081479+Fransssss@users.noreply.github.com>
2022-06-20 09:48:28 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# loop control statement
+# Break    - stop loop
+
+print("\nLoop Control Statement")
+print("1 X\n2 X\n3 X\nQ quit")
+choice = input("Choose one from these 3 numbers to answer a question: ")
+
+while choice != "Q" and choice != "q":     # while input is not q/Q
+    if choice == "1":
+        name = input("\nWhat is your name: ")
+        print("Hello " + name)
+        break                              # get out of the loop - no need to wait until user input q/Q
+    elif choice == "2":
+        age = int(input("\nHow old are you: "))
+        print("You are " + str(age) + " y/d")
+        break
+    elif choice == "3":
+        color = input("\nwhat is your favorite color: ")
+        print("Your favorite color is " + color)
+    else:
+        print("\n[Invalid choice]\n")
+
+    print("1 X\n2 X\n3 X\nQ quit")
+    choice = input("Choose one from these 3 numbers to answer a question: ")
